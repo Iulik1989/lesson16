6195d5857555761e9f9f507275b210ba8c5ef957
Iulik <inicula1989@gmail.com>
2024-03-13 21:01:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i, 'hi')
+// }
+
+// for(;;){
+//     console.log('hi')
+// }
+
+// for (let i = 10; i >=1; i-=2) console.log('hi', i);
+
+// let i = 10;
+
+// while(--i) {
+//     console.log('hi', i)
+    
+// }
+
+// do {
+//     console.log('hi', i)
+// } while(i > 20)
+
+
+
+// console.log(value)
+
+// const value = 5;
+
+// console.log(value)
+
+// let i = 10;
+
+// function func() {
+//     let i = 20 
+//     console.log(i)
+//     function func1() {
+            
+//         console.log(i);
+//         i = 15;
+//     }
+//     func1()
+//     console.log(i)
+// }
+
+// func()
+
+// console.log(i)
+
+// const condition = false
+
+// if(condition){
+//     let i = 10;
+// }
+
+// console.log(i)
+
+
+// const arrowFunc = (nume) => 'Hi ' + nume
+
+
+// console.log(arrowFunc('Alex'))
+
+// const arrowFunc = (nume) => {
+//     console.log(arguments)
+//     return 'Hi ' + nume
+// }
+
+
+// console.log(arrowFunc('Alex'))
+
+// console.log(funcDeclaration('Alex', 'Victor', 'ssdf', 'Ion'))
+
+// function funcDeclaration(nume) {
+//     console.log(arguments)
+//     return 'Hi ' + nume
+// }
+
+// const condition = false
+
+// const funcExpression = function (nume) {
+//     if(condition) {
+//         return 'first case'
+//     } else {
+//         return 'second case'
+//     }
+// }
+
+// const funcResult = funcExpression()
+
+// console.log(funcResult)
+
+
+
+
